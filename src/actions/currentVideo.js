ba2c71edaf6ceb4384715fe5585452090ca20604
Xue Chen <xueqingc@gmail.com>
2018-07-10 18:38:52 -0700

    complete actions - currentsVideo, search and videoList

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,8 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+   type: "CHANGE_VIDEO",
+   video
+
 });
 
 export default changeVideo;
--- src/actions/search.js
+++ src/actions/search.js
@@ -5,7 +5,13 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+  return (dispatch) => {
+
+    searchYouTube({key: YOUTUBE_API_KEY, query: q}, (items) => {
+      dispatch(changeVideo(items[0]))
+      dispatch(changeVideoList(items))
+    })
+  } 
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
